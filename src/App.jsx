2bb65a8a468fcd8067a689aee4c2b866b9c5d4e5
kmTyagi198 <k.m.tyagi198@gmail.com>
2026-02-05 00:01:46 +0530

    created board logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Block from './components/Block'
+import PlayGround from './components/PlayGround'
 
 export default function App() {
     return (
         <div className='flex justify-center w-screen h-screen '>
-            <div className='border-2 w-90 h-180 m-2 items-center'>
+            {/* <div className='border-2 w-90 h-180 m-2 items-center'>
                     <Block/>
-            </div>
+            </div> */}
+            <PlayGround/>
 
         </div>
     )

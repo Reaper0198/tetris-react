437490e0bcd502456a113e4f3e3242c76d1df20b
kmTyagi198 <k.m.tyagi198@gmail.com>
2026-02-06 23:09:50 +0530

    seperated render logic from jsx

--- src/components/PlayGround.jsx
+++ src/components/PlayGround.jsx
@@ -1,37 +1,37 @@
 import { useState } from "react"
-import {checkCollision, L} from './block.js'
+import { checkCollision, L } from './block.js'
 
 export default function PlayGround() {
 
 
 
     const initialPiece = {
-        grid : L,
-        x_pos : 4,
-        y_pos : 0
+        grid: L,
+        x_pos: 4,
+        y_pos: 0
     }
 
-    const initialBoard = 
-    [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+    const initialBoard =
+        [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
 
     //     [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     // [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -59,51 +59,68 @@ export default function PlayGround() {
     const [piece, setPiece] = useState(initialPiece);
 
     const handleKeyPress = (e) => {
-        console.log(e.key);
-        if(e.key === 'd'){
-            if(checkCollision(board, piece.grid, (piece.x_pos + 1), (piece.y_pos))){
-                setPiece(prev => ({ ...prev, x_pos : (prev.x_pos+1)}))
+        // console.log(e.key);
+        if (e.key === 'd') {
+            if (checkCollision(board, piece.grid, (piece.x_pos + 1), (piece.y_pos))) {
+                setPiece(prev => ({ ...prev, x_pos: (prev.x_pos + 1) }))
             }
-        }else if(e.key === 'a'){
-            if(checkCollision(board, piece.grid, (piece.x_pos - 1), (piece.y_pos))){
-                setPiece(prev => ({ ...prev, x_pos : (prev.x_pos-1)}))
+        } else if (e.key === 'a') {
+            if (checkCollision(board, piece.grid, (piece.x_pos - 1), (piece.y_pos))) {
+                setPiece(prev => ({ ...prev, x_pos: (prev.x_pos - 1) }))
             }
-        }else if(e.key === 's'){
-            if(checkCollision(board, piece.grid, (piece.x_pos), (piece.y_pos + 1))){
-                setPiece(prev => ({ ...prev, y_pos : (prev.y_pos+1)}))
+        } else if (e.key === 's') {
+            if (checkCollision(board, piece.grid, (piece.x_pos), (piece.y_pos + 1))) {
+                setPiece(prev => ({ ...prev, y_pos: (prev.y_pos + 1) }))
             }
-        }else if(e.key === 'w'){
-            if(checkCollision(board, piece.grid, (piece.x_pos ), (piece.y_pos - 1))){
-                setPiece(prev => ({ ...prev, y_pos : (prev.y_pos-1)}))
+        } else if (e.key === 'w') {
+            if (checkCollision(board, piece.grid, (piece.x_pos), (piece.y_pos - 1))) {
+                setPiece(prev => ({ ...prev, y_pos: (prev.y_pos - 1) }))
             }
         }
     }
 
+    const generateFinalBoardArray = (board, piece) => {
+        const finalBoard = board.map(row => [...row]);
+
+        piece.grid.map((row, y) => {
+            row.map((cell, x) => {
+                if (cell === 1) {
+
+                    const piece_x = piece.x_pos + x;
+                    const piece_y = piece.y_pos + y;
+
+                    if (piece_x >= 0 && piece_x < board[0].length &&
+                        piece_y >= 0 && piece_y < board.length) {
+                        finalBoard[piece_y][piece_x] = 2;
+                    }
+                }
+            })
+        })
+        return finalBoard;
+    }
+
+    const finalBoard = generateFinalBoardArray(board, piece);
+
     return (
         <>
-            <div className="w-90 h-180 border grid grid-cols-10 m-2"
+            <div className="w-90 h-180 border grid grid-cols-10 m-2 p-2"
                 onKeyDown={handleKeyPress} tabIndex={2}
             >
                 {
-                    board.map((row,rowIndex) => {
+                    finalBoard.map((row, rowIndex) => {
                         return (row.map((cell, cellIndex) => {
-                            if(cell == 1){
-                                return (<div key={`${rowIndex}-${cellIndex}`} className="w-8 h-8 bg-gray-700"></div>)
-                            }else{
-                                if((rowIndex - piece.y_pos) < piece.grid.length &&
-                                    (cellIndex - piece.x_pos) < piece.grid[0].length && 
-                                      (rowIndex - piece.y_pos) >= 0 && 
-                                        (cellIndex - piece.x_pos) >= 0){
-                                        if(piece.grid[rowIndex - piece.y_pos][cellIndex - piece.x_pos] == 1){
-                                            return (<div key={`${rowIndex}-${cellIndex}`} className="w-8 h-8 bg-amber-600"></div>)
-                                        }
-                                    }
-                                }
-                                return (<div key={`${rowIndex}-${cellIndex}`} className="w-8 h-8 bg-gray-500"></div>)
+                            if (cell == 1) {
+                                return (<div key={`${rowIndex}-${cellIndex}`} className="w-8 h-8 border bg-gray-700"></div>)
+                            } else if (cell == 2) {
+                                return (<div key={`${rowIndex}-${cellIndex}`} className="w-8 h-8 border bg-amber-600"></div>)
+                            } else if (cell == 0) {
+                                return (<div key={`${rowIndex}-${cellIndex}`} className="w-8 h-8 border bg-gray-500"></div>)
+                            }
                         })
-                    )})
+                        )
+                    })
                 }
-            
+
             </div>
         </>
     )

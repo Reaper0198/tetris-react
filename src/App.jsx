41f1c24392d6bdd92d17415eab74a2a2d411bafe
kmTyagi198 <k.m.tyagi198@gmail.com>
2026-02-13 22:57:33 +0530

    added UI for play/pause and reset

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { useEffect, useRef, useState } from 'react';
 import './App.css'
-import PlayGround from './components/PlayGround'
+import PlayGround from './components/PlayGround';
+
+import { IoPlayOutline } from "react-icons/io5";
+import { IoPauseOutline } from "react-icons/io5";
+import { RiResetRightLine } from "react-icons/ri";
 
 export default function App() {
 
     const [level, setLevel] = useState(1);
     const [score, setScore] = useState(0);
+    const [runGame, setRunGame] = useState(false);
 
     const tickRef = useRef(1000);
 
@@ -24,6 +29,10 @@ export default function App() {
         })
     }
 
+    const handleRunGame = () => {
+        setRunGame(prev => !prev);
+    }
+
     useEffect(() => {
         console.log(level);
     }, [level])
@@ -33,6 +42,16 @@ export default function App() {
             <div className='flex flex-col flex-1 py-40 items-center'> 
 
                 <p className='font-doto text-8xl font-black'>TETRIS</p>
+                <div className='flex gap-2'>
+                    <div className='border-4 py-3 px-7 rounded-lg'
+                        onClick={handleRunGame}>
+                        {runGame ? <IoPauseOutline  size={50}/> :
+                        <IoPlayOutline size={50}/> }
+                    </div>
+                    <div className='border-4 py-3 px-7 rounded-lg'>
+                        <RiResetRightLine size={50}/>
+                    </div>
+                </div>
 
 
             </div>

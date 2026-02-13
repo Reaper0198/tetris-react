16d4ecf99e71a13950b7144c832577134daf5d55
kmTyagi198 <k.m.tyagi198@gmail.com>
2026-02-13 22:25:26 +0530

    created structure for UI

--- src/App.jsx
+++ src/App.jsx
@@ -13,35 +13,40 @@ export default function App() {
         setScore(prev => {
             const newScore = prev + addScore;
 
-            if(Math.floor((newScore / 500)) !== Math.floor((prev / 500))){
+            if (Math.floor((newScore / 500)) !== Math.floor((prev / 500))) {
                 setLevel(prev => {
-                    console.log(prev+1)
-                    return (prev+1)
+                    console.log(prev + 1)
+                    return (prev + 1)
                 });
-            }    
+            }
 
             return newScore;
         })
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         console.log(level);
     }, [level])
 
     return (
         <div className='flex justify-evenly w-screen h-screen '>
+            <div className='flex flex-col flex-1 py-40 items-center'> 
+
+                <p className='font-doto text-8xl font-black'>TETRIS</p>
+
+
+            </div>
             <div>
+                <PlayGround
+                    level={level}
+                    increaseScoreBy={increaseScoreBy} />
+            </div>
+            <div className='flex flex-col items-center flex-1'>
                 score = {score}
                 <br />
                 level = {level}
                 <br />
                 tick lenght = {tickRef.current}
-            </div>
-            <PlayGround
-                level={level}
-                increaseScoreBy={increaseScoreBy} />
-            <div>
-
             </div>
         </div>
     )
